94ce158281f5572c9de0cbc071f9c672e363c36f
Geraint Luff <luffgd@gmail.com>
2013-11-02 17:59:38 +0000

    Update UI

--- renderers/ane/question.js
+++ renderers/ane/question.js
@@ -2,38 +2,37 @@ var lang;
 
 // Render translated text
 Jsonary.render.register({
-    renderHtml: function(data) {
-	if (!lang) { lang = 'de'; }
+	renderHtml: function(data) {
+		lang = lang || 'de';
+		var result = '<div class="translation">';
 
-	var jq = $('<div class="translation"><div class="foreign"></div><div class="english"></div></div>');
-	jq.find('.foreign').text(data.propertyValue(lang) || "(no translation)");
-	jq.find('.english').text(data.propertyValue('en'));
-	console.log(jq);
-	return jq.html();
-    },
-    
-    filter: {
-    	readOnly: true,
-    	schema: "question.json#/definitions/message"
-    }
+		result += '<div class="foreign">' + Jsonary.escapeHtml(data.propertyValue(lang)) + '</div>';
+		result += '<div class="english">' + Jsonary.escapeHtml(data.propertyValue('en')) + '</div>';
+		return result + '</div>';
+	},
+
+	filter: {
+		readOnly: true,
+		schema: "question.json#/definitions/message"
+	}
 });
 
 
 // Render a question
 
 Jsonary.render.register({
-    renderHtml: function(data, cx) {
-	result = '<div class="question">'+cx.renderHtml(data.property("questionText"))+'</div>';
+	renderHtml: function(data, cx) {
+		result = '<div class="question">'+cx.renderHtml(data.property("questionText"))+'</div>';
 
-	var answers = data.property("answers");
-	for(var i=0; i<answers.length(); i++) {
-	    result += '<div class="answer">'+cx.renderHtml(answers.item(i))+'</div>';
+		var answers = data.property("answers");
+		for(var i=0; i<answers.length(); i++) {
+			result += '<div class="answer">'+cx.renderHtml(answers.item(i))+'</div>';
+		}
+
+		return result;
+	},
+	filter: {
+		readOnly: true,
+		schema: "question.json"
 	}
-
-	return result;
-    },
-    filter: {
-        readOnly: true,
-        schema: "question.json"
-    }
 });
